8b272bf739d2bee77b200474f6b83e1fbf5c58cb
EXploringEA <adrian@exploringea.co.uk>
2017-07-01 16:30:14 +0100

    Updated licence and sandcastle documentation

--- 1 Forms/frmInspector.Designer.vb
+++ 1 Forms/frmInspector.Designer.vb
@@ -1,9 +1,13 @@
 ﻿' Copyright (C) 2015 - 2017 Adrian LINCOLN, EXploringEA - All Rights Reserved
-' You may use, distribute and modify this code under the terms of the 3-Clause BSD License
 '
-' You should have received a copy of the 3-Clause BSD License with this file. 
-' If not, please email: adrian@EXploringEA.co.uk 
-'=====================================================================================
+'   This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by
+'   the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+'   This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+'   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+'
+'    You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+' =============================================================================================================================================
 <Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
 Partial Class frmInspector
     Inherits System.Windows.Forms.Form
--- 1 Forms/frmListOfClasses.vb
+++ 1 Forms/frmListOfClasses.vb
@@ -1,4 +1,14 @@
-﻿''' <summary>
+﻿' Copyright (C) 2015 - 2017 Adrian LINCOLN, EXploringEA - All Rights Reserved
+'
+'   This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by
+'   the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+'   This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+'   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+'
+'    You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+' =============================================================================================================================================
+''' <summary>
 ''' Form to display a list of public classes and methods for a selected AddIn entry (DLL)
 ''' </summary>
 ''' <seealso cref="System.Windows.Forms.Form" />
@@ -10,9 +20,9 @@ Public Class frmListOfClasses
     ''' </summary>
     Private ListOfClasses As New ArrayList
     ''' <summary>
-    ''' Initializes a new instance of the <see cref="frmListOfMethods"/> class.
+    ''' Initializes a new instance of the <see cref="frmListOfClasses"/> class.
     ''' </summary>
-    ''' <param name="pListOfMethods">list of methods.</param>
+    ''' <param name="pListOfClasses">list of methods.</param>
     Public Sub New(ByRef pListOfClasses As ArrayList)
         InitializeComponent()
         ListOfClasses = pListOfClasses
